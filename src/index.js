10a1f96df41b7026c7093d019685d3f116503652
Abdeali <abdealidahodwala163@gmail.com>
2024-08-03 13:47:37 +0530

    FEAT : Rate limiting and Reverse Proxy setup completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-const express = require('express');
+const express = require("express");
 
-const { ServerConfig } = require('./config');
-const apiRoutes = require('./routes');
+const { ServerConfig } = require("./config");
+const apiRoutes = require("./routes");
+const rateLimit = require("express-rate-limit");
+const {createProxyMiddleware} = require("http-proxy-middleware");
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use('/api', apiRoutes);
+app.use(express.urlencoded({ extended: true }));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+});
+
+app.use(limiter); //rate limiter
+
+//reverse Proxy
+app.use("/flightsService",createProxyMiddleware({target: "http://localhost:3000/",changeOrigin: true,pathRewrite: {'^/flightsService' : '/'}}));
+app.use("/bookingsService",createProxyMiddleware({target: "http://localhost:4000/",changeOrigin: true,pathRewrite: {'^/bookingsService' : '/'}}));
+
+app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
-    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+  console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 });
